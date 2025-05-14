5fe7e9a84cffd4f60494e7a085a7478ac39884e6
Toan Nguyen <codehub.personal@gmail.com>
2025-05-14 13:01:05 +0700

    init backend proj

---
+++ backend/lemongrass/src/main/java/com/ttoannguyen/lemongrass/LemongrassApplication.java
@@ -0,0 +1,13 @@
+package com.ttoannguyen.lemongrass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemongrassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemongrassApplication.class, args);
+	}
+
+}
---
+++ backend/lemongrass/src/test/java/com/ttoannguyen/lemongrass/LemongrassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttoannguyen.lemongrass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemongrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
