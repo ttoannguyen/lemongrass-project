92623b6df09ca6c7510e13d1631e4448a99eaf25
Toan Nguyen <codehub.personal@gmail.com>
2025-07-16 23:39:04 +0700

    update - fix: Postresponse, postmapper

--- lemongrass/src/main/java/com/ttoannguyen/lemongrass/dto/response/post/PostResponse.java
+++ lemongrass/src/main/java/com/ttoannguyen/lemongrass/dto/response/post/PostResponse.java
@@ -1,12 +1,14 @@
 package com.ttoannguyen.lemongrass.dto.Response.post;
 
 import com.ttoannguyen.lemongrass.dto.Response.group.GroupResponse;
+import com.ttoannguyen.lemongrass.dto.Response.image.ImageResponse;
 import com.ttoannguyen.lemongrass.dto.Response.recipe.RecipeResponse;
 import com.ttoannguyen.lemongrass.dto.Response.account.AccountResponse;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -19,6 +21,7 @@ public class PostResponse {
   String content;
   String visibility;
   boolean isApproved;
+  List<ImageResponse> images;
   AccountResponse author;
   GroupResponse group;
   RecipeResponse recipe;
--- lemongrass/src/main/java/com/ttoannguyen/lemongrass/mapper/PostMapper.java
+++ lemongrass/src/main/java/com/ttoannguyen/lemongrass/mapper/PostMapper.java
@@ -14,21 +14,24 @@ import org.mapstruct.Named;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = { AccountMapper.class, GroupMapper.class, RecipeMapper.class })
+@Mapper(
+    componentModel = "spring",
+    uses = {AccountMapper.class, GroupMapper.class, RecipeMapper.class, ImageMapper.class})
 public interface PostMapper {
-    Post toPost(PostCreateRequest request);
+  Post toPost(PostCreateRequest request);
 
-    @Mapping(source = "account", target = "author")
-    @Mapping(source = "group", target = "group")
-    @Mapping(source = "recipe", target = "recipe")
-    @Mapping(source = "approved", target = "isApproved")
-    @Mapping(source = "visibility", target = "visibility", qualifiedByName = "visibilityToString")
-    PostResponse toPostResponse(Post post);
+  @Mapping(source = "account", target = "author")
+  @Mapping(source = "group", target = "group")
+  @Mapping(source = "recipe", target = "recipe")
+  @Mapping(source = "approved", target = "isApproved")
+  @Mapping(source = "visibility", target = "visibility", qualifiedByName = "visibilityToString")
+  @Mapping(source = "images", target = "images")
+  PostResponse toPostResponse(Post post);
 
-    @Named("visibilityToString")
-    default String visibilityToString(Visibility visibility) {
-        return visibility != null ? visibility.name() : null;
-    }
+  @Named("visibilityToString")
+  default String visibilityToString(Visibility visibility) {
+    return visibility != null ? visibility.name() : null;
+  }
 
-    List<PostResponse> toListPostResponse(List<Post> posts);
+  List<PostResponse> toListPostResponse(List<Post> posts);
 }
